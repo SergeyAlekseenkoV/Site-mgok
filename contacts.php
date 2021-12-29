2c96fd5573e2aa892f438fabf8f9486d1dfe7877
SergeyAlekseenko <alexsvmodest@gmail.com>
2021-12-29 21:50:09 +0200

    fixed banner-height

---
+++ contacts.php
@@ -0,0 +1,107 @@
+<?php include_once "header.php"; ?>
+<title>МГЗК-Контакти</title>
+</head>
+
+<body>
+    <?php include_once "menu.php"; ?>
+    <!-- main-section-->
+    <main class="main mt-0 pb-2 pb-lg-4 position-relative">
+        <!-- banner -->
+        <section class="height-content banner-shape">
+            <figure class="banner-container position-relative mb-0">
+                <img src="/build/img/Contact Us.jpg" alt="баннер" class="w-100 banner-img img-fluid obj-fit position-relative">
+                <figcaption class="banner-title-container position-absolute d-flex flex-column justify-content-center bg_dark_through w-100 min-height">
+                    <h1 class="container pl-md-0 text-white display-4 mb-5">
+                        <span class="blur shadow-dark">Контакти</span>
+                    </h1>
+                </figcaption>
+            </figure>
+        </section>
+        <!-- content -->
+        <section class="content bg-transparent d-flex flex-column fustify-content-between align-items-center mx-3 mx-sm-0 mt-3 mt-lg-4">
+            <article class="container mx-0 border px-0 mb-3">
+                <div class="row mx-0 p-3">
+                    <div class="col-12 col-md-4 p-3">
+                        <h2 class="content__h mb-3 d-md-none">Головний офіс</h2>
+                        <img src="/build/img/head-office.jpg" alt="офіс" class="img-fluid border border-primary d-none d-md-block">
+                    </div>
+                    <div class="col-12 col-md-8 p-3 text-right d-flex flex-column justify-content-center align-items-center">
+                        <div class="w-100 ukrainian">
+                            <p>Українською мовою:
+                                <br>Акціонерне товариство «Марганецький гірничо-збагачувальний комбінат»
+                                <br>(скор. АТ «Марганецький ГЗК»)
+                            </p>
+                            <p class="lead mb-0 font-weight-bold">Юридична та фактична адреса:</p>
+                            <p class="lead">
+                            <address>Україна, 53400, м. Марганець, Дніпропетровської обл., вул.Єдності,62</address>
+                            </p>
+                            <p class="lead mb-0">
+                                <span>
+                                    <i class="fas fa-phone-volume"></i>
+                                    +38(056)790-16-35
+                                </span>
+                            </p>
+                        </div>
+                        <br>
+                        <div class="w-100 russian">
+                            <p>На русском:
+                                <br>Акционерное общество «Марганецкий горно-обогатительный комбинат» <br>(сокр. АО «Марганецкий ГОК»)
+                            </p>
+                            <p class="lead mb-0 font-weight-bold">Юридический и фактический адрес:</p>
+                            <p class="lead">
+                            <address>Украина, 53400, г. Марганец, Днепропетровской обл., ул. Единства,62, ЕДРПОУ 00190911</address>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <article class="container mx-0 border px-0 mb-3">
+                <p class="p-3 mb-0">
+                    р/рахунок <strong>26005000000677</strong> АТ "БАНК КРЕДИТ ДНІПРО" в м.Київ <strong>МФО 305749 SWIFT: CRDEUA2N</strong>
+                </p>
+                <p class="p-3 mb-0">
+                    р/р <strong>26001010049116 </strong> АБ "Південний" в м.Одеса <strong>МФО 328209, SWIFT: PIVDUA22</strong>
+                </p>
+                <p class="p-3 mb-0">
+                    Код ЄДРПОУ <strong>00190911</strong> ІПН <strong>001909104062</strong> № свідоцтва <strong>100335270</strong>
+                </p>
+            </article>
+            <div class="mails container px-0 d-flex flex-wrap justify-content-between border mb-3">
+                <article class="flex-fill mx-0 mb-3 px-0">
+                    <p class="px-3 pt-3 pb-1 mb-0">
+                        <strong>Офіційна адреса електронної пошти:</strong>
+                    </p>
+                    <p class="lead font-weight-bold px-3 py-1"><span class="text-primary"><i class="fas fa-mail-bulk"></i></span>
+                        <a href="mailto:mgzk.info@gmail.com" class="text_color">mgzk.info@gmail.com</a>
+                    </p>
+                </article>
+                <article class="flex-fill mx-0 mb-3 px-0">
+                    <p class="px-3 pt-3 pb-1 mb-0">
+                        <strong>Голова правління:</strong>
+                    </p>
+                    <p class="lead font-weight-bold px-3 py-1 mb-0"><span class="text-primary"><i class="fas fa-mail-bulk"></i></span>
+                        <a href="mailto:secretary@mgok.dp.ua" class="text_color">secretary@mgok.dp.ua</a>
+                    </p>
+                </article>
+                <article class="flex-fill mx-0 mb-3 px-0">
+                    <p class="px-3 pt-3 pb-1 mb-0">
+                        <strong>Начальник бюро збуту і ЗЕД:</strong>
+                    </p>
+                    <p class="lead font-weight-bold px-3 py-1 mb-0"><span class="text-primary"><i class="fas fa-mail-bulk"></i></span>
+                        <a href="mailto:nosbut@mgok.dp.ua" class="text_color">nosbut@mgok.dp.ua</a>
+                    </p>
+                </article>
+                <article class="flex-fill mx-0 mb-3 px-0">
+                    <p class="px-3 pt-3 pb-1 mb-0">
+                        <strong>Референт голови правління:</strong>
+                    </p>
+                    <p class="lead font-weight-bold px-3 py-1 mb-0"><span class="text-primary"><i class="fas fa-mail-bulk"></i></span>
+                        <a href="mailto:referent@mgok.dp.ua" class="text_color">referent@mgok.dp.ua</a>
+                    </p>
+                    <p class="lead p-3 mb-0"><span class="text-primary mr-3"><i class="fas fa-phone-alt"></i></span>(056) 790-16-33</p>
+                </article>
+            </div>
+        </section>
+    </main>
+    <!--footer-->
+    <?php include_once "footer.php"; ?>
---
+++ menu.php
@@ -0,0 +1,251 @@
+    <header class="main-header p-0">
+        <div class="social-top px-3 px-xl-0 bg-light">
+            <div class="container px-0 d-flex flex-row flex-wrap py-1 justify-content-end align-items-center">
+                <div class="m-0 position-absolute search_bar_wrapper">
+                    <form class="search_bar position-relative">
+                        <input type="text" name="search" class="search_bar_input position-absolute shadow" maxlength="70" placeholder="Пошук" id="searchInput" aria-label="search">
+                        <button type="submit" class="btn search_bar_submit" aria-label="submit search">
+                            <i class="fas fa-search"></i></button>
+                    </form>
+                </div>
+                <ul class="list-unstyled d-flex flex-row justify-content-end align-items-center mb-0">
+                    <li>
+                        <a href="https://www.facebook.com/infoMGOK/?modal=admin_todo_tour" target="_blank" rel="nofollow noopener" class="lead py-2 pt-0 text-primary"><i class="fab fa-facebook-square"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://www.youtube.com/channel/UClwIjrpbN7aFZZVFoY3Bi-Q" target="_blank" rel="nofollow noopener" class="lead pl-2 pt-0 pr-0 text-primary"><i class="fab fa-youtube"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!--navigation-->
+        <nav class="navbar navbar-dark navbar-expand-lg shadow py-0 px-3 px-xl-0 position-relative blur">
+            <div class="container menu-container px-0">
+                <div class="d-flex flex-row justify-content-start align-items-center right-shaped m-0 position-relative">
+                    <div class="d-flex flex-row width-content text_color position-absolute">
+                        <div class="logo m-0 pl-0 pl-lg-1 d-flex justify-content-center align-items-center rounded-circle width-content">
+                            <a href="index.php" class="align-self-center">
+                                <img src="../build/img/logo-dark.png" width="40" height="40" alt="MGOK" class="inner-logo bg-light">
+                            </a>
+                        </div>
+                        <abbr class="font-weight-bold d-none d-sm-inline-block ml-0 ml-md-2 ml-lg-2 ml-xl-2 text-logo shadow-dark"><span class="d-none d-xl-inline-block">AT</span>&nbsp;"МГЗК"</abbr>
+                    </div>
+                </div>
+                <div class="invisible"></div>
+                <!--menu button-->
+                <button class="navbar-toggler border border-transparent collapsed position-relative p-0" type="button" id='togglerMenu'>
+                    <span class="icon-bar" data-action='mobileMenu'></span>
+                    <span class="icon-bar" data-action='mobileMenu'></span>
+                    <span class="icon-bar" data-action='mobileMenu'></span>
+                </button>
+                <!--dropdowns lg-->
+                <div class="collapse navbar-collapse text-center d-none d-lg-block">
+                    <ul class="navbar-nav nav-pills ml-0 ml-md-auto my-0">
+                        <li class="d-flex justify-content-center nav-item position-relative">
+                            <a class="text-uppercase nav-link text-white py-3 px-2 px-lg-4 shadow-dark" href="history.php" role="button" aria-haspopup="true" aria-expanded="false" tabindex="1">Історія</a>
+                        </li>
+                        <li class="nav-item d-flex justify-content-center position-relative" data-name="enterprize">
+                            <a class="text-uppercase nav-link py-3 px-lg-4 text-white shadow-dark" href="#" role="button" aria-haspopup="true" aria-expanded="false" tabindex="2">Підприємство</a>
+                        </li>
+                        <li class="d-flex justify-content-center nav-item position-relative" data-name="consumers">
+                            <a class="text-uppercase nav-link text-white py-3 px-2 px-lg-4 shadow-dark" href="#" role="button" aria-haspopup="true" aria-expanded="false" tabindex="10">Споживачам</a>
+                        </li>
+                        <li class="d-flex justify-content-center nav-item position-relative" data-name="partners">
+                            <a class="text-uppercase nav-link text-white py-3 px-2 px-lg-4 shadow-dark" href="#" role="button" aria-haspopup="true" aria-expanded="false" tabindex="16">Партнерам</a>
+                        </li>
+                        <li class="d-flex justify-content-center nav-item position-relative">
+                            <a class="text-uppercase nav-link text-white py-3 px-2 px-lg-4 shadow-dark" href="#" role="button" aria-haspopup="true" aria-expanded="false" tabindex="20">Продукція</a>
+                        </li>
+                        <li class="d-flex justify-content-center nav-item position-relative">
+                            <a class="text-uppercase nav-link text-white py-3 px-2 px-lg-4 shadow-dark" href="#" role="button" aria-haspopup="true" aria-expanded="false" tabindex="21">Портал</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- dropdown block -->
+            <div class="w-100 d-none py-0 justify-content-center align-items-center dropdown-block px-0 position-absolute bg_light_through shadow blur">
+                <ul class="container d-none justify-content-center align-items-center list-unstyled my-1 name-list" data-name="enterprize">
+                    <li>
+                        <a href="#" class="px-5 py-2 py-xl-3" tabindex="3">МГЗК сьогодні</a>
+                    </li>
+                    <li>
+                        <a href="#" class="px-5 py-2 py-xl-3" tabindex="4">Охорона праці</a>
+                    </li>
+                    <li>
+                        <a href="#" class="px-5 py-2 py-xl-3" tabindex="5">Профспілка</a>
+                    </li>
+                    <li>
+                        <a href="#" class="px-5 py-2 py-xl-3" tabindex="6">Комбінат-місту</a>
+                    </li>
+                    <li>
+                        <a href="#" class="px-5 py-2 py-xl-3" tabindex="7">Соціальна сфера</a>
+                    </li>
+                    <li>
+                        <a href="#" class="px-5 py-2 py-xl-3" tabindex="8">Вакансії</a>
+                    </li>
+                    <li>
+                        <a href="contacts.php" class="px-5 py-2 py-xl-3" tabindex="9">Контакти</a>
+                    </li>
+                </ul>
+                <ul class="container d-none justify-content-center align-items-center list-unstyled my-1 name-list" data-name="consumers">
+                    <li>
+                        <a href="#" class="px-5 py-2 py-xl-3" tabindex="11">Нормативні документи</a>
+                    </li>
+                    <li>
+                        <a href="#" class="px-5 py-2 py-xl-3" tabindex="12">Кол-центр</a>
+                    </li>
+                    <li>
+                        <a href="#" class="px-5 py-2 py-xl-3" tabindex="13">Комерційні пропозиції</a>
+                    </li>
+                    <li>
+                        <a href="#" class="px-5 py-2 py-xl-3" tabindex="14">Договори</a>
+                    </li>
+                    <li>
+                        <a href="#" class="px-5 py-2 py-xl-3" tabindex="15">Форми звернення</a>
+                    </li>
+                </ul>
+                <ul class="container d-none justify-content-center align-items-center list-unstyled my-1 name-list" data-name="partners">
+                    <li>
+                        <a href="#" class="px-5 py-2 py-xl-3" tabindex="17">Акціонери</a>
+                    </li>
+                    <li>
+                        <a href="#" class="px-5 py-2 py-xl-3" tabindex="18">Тендери</a>
+                    </li>
+                    <li>
+                        <a href="#" class="px-5 py-2 py-xl-3" tabindex="19">Важлива інформація</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <!-- mobile menu -->
+    <section class="menu-sm position-absolute flex-column fustify-content-center align-items-center bg_light_through pt-3 pt-md-5 blur">
+        <ul class="mx-auto accordion container px-0 text-center list-unstyled bg_dark_through" id="accordionMenu">
+            <li class="card">
+                <div class="card-header">
+                    <h2 class="mb-0">
+                        <a href="history.php" class="btn btn-link btn-block text-left text-uppercase" role="button">
+                            Історія
+                        </a>
+                    </h2>
+                </div>
+            </li>
+            <li class="card">
+                <div class="card-header" id="headingOne">
+                    <h2 class="mb-0">
+                        <button class="btn btn-link btn-block text-left text-uppercase" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Підприємство<span>&nbsp;&nbsp;<i class="fas fa-chevron-down"></i></span>
+                        </button>
+                    </h2>
+                </div>
+                <div id="collapseOne" class="collapse w-75 mx-auto" aria-labelledby="headingOne" data-parent="#accordionMenu">
+                    <ul class="card-body list-unstyled px-0">
+                        <li>
+                            <a href="#" class="p-3 btn btn-block my-1">МГЗК сьогодні</a>
+                        </li>
+                        <li>
+                            <a href="#" class="p-3 btn btn-block my-1">Охорона праці</a>
+                        </li>
+                        <li>
+                            <a href="#" class="p-3 btn btn-block my-1">Профспілка</a>
+                        </li>
+                        <li>
+                            <a href="#" class="p-3 btn btn-block my-1">Комбінат-місту</a>
+                        </li>
+                        <li>
+                            <a href="#" class="p-3 btn btn-block my-1">Соціальна сфера</a>
+                        </li>
+                        <li>
+                            <a href="#" class="p-3 btn btn-block my-1">Вакансії</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="card">
+                <div class="card-header">
+                    <h2 class="mb-0">
+                        <a href="#" class="btn btn-link btn-block text-left text-uppercase" role="button">
+                            Портал МГЗК
+                        </a>
+                    </h2>
+                </div>
+            </li>
+            <li class="card">
+                <div class="card-header" id="headingTwo">
+                    <h2 class="mb-0">
+                        <button class="btn btn-link btn-block text-left collapsed text-uppercase" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            Споживачам<span>&nbsp;&nbsp;<i class="fas fa-chevron-down"></i></span>
+                        </button>
+                    </h2>
+                </div>
+                <div id="collapseTwo" class="collapse w-75 mx-auto" aria-labelledby="headingTwo" data-parent="#accordionMenu">
+                    <ul class="card-body list-unstyled px-0">
+                        <li>
+                            <a href="#" class="p-3 btn btn-block my-1">Нормативні документи</a>
+                        </li>
+                        <li>
+                            <a href="#" class="p-3 btn btn-block my-1">Кол-центр</a>
+                        </li>
+                        <li>
+                            <a href="#" class="p-3 btn btn-block my-1">Комерційні пропозиції</a>
+                        </li>
+                        <li>
+                            <a href="#" class="p-3 btn btn-block my-1">Договори</a>
+                        </li>
+                        <li>
+                            <a href="#" class="p-3 btn btn-block my-1">Форми звернення</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="card">
+                <div class="card-header" id="headingThree">
+                    <h2 class="mb-0">
+                        <button class="btn btn-link btn-block text-left collapsed text-uppercase" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Партнерам<span>&nbsp;&nbsp;<i class="fas fa-chevron-down"></i></span>
+                        </button>
+                    </h2>
+                </div>
+                <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionMenu">
+                    <ul class="card-body list-unstyled px-0">
+                        <li>
+                            <a href="#" class="p-3 btn btn-block my-1">Акціонерам</a>
+                        </li>
+                        <li>
+                            <a href="#" class="p-3 btn btn-block my-1">Тендери</a>
+                        </li>
+                        <li>
+                            <a href="#" class="p-3 btn btn-block my-1">Важлива інформація</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="card">
+                <div class="card-header">
+                    <h2 class="mb-0">
+                        <a href="#" class="btn btn-link btn-block text-left text-uppercase" role="button">
+                            Продукція
+                        </a>
+                    </h2>
+                </div>
+            </li>
+            <li class="card">
+                <div class="card-header">
+                    <h2 class="mb-0">
+                        <a href="#" class="btn btn-link btn-block text-left text-uppercase" role="button">
+                            Вакансії
+                        </a>
+                    </h2>
+                </div>
+            </li>
+            <li class="card">
+                <div class="card-header">
+                    <h2 class="mb-0">
+                        <a href="contacts.php" class="btn btn-link btn-block text-left text-uppercase" role="button">
+                            Контакти
+                        </a>
+                    </h2>
+                </div>
+            </li>
+        </ul>
+    </section>
